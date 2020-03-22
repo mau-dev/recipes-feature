9a17de81416ff7ac6de98e99f9f6f207c14bc4d3
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2020-03-22 17:21:18 +0800

    adding auth middleware, verify token

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
-// @route    GET api/auth
-//@desc      Test route
-//@access    Public
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
 
-router.get('/', (req, res) => res.send('Auth route'));
+// @route    GET api/auth
+//@desc      GET user by token
+//@access    Private
+
+//protect the route with the auth middleware
+router.get('/', auth, async (req, res) => {
+    //change the route to return user's data
+    try {
+        //take the user model
+        const user = await User.findById(req.user.id).select('-password');
+        //send the user as response
+        res.json(user);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 
 module.exports = router;
