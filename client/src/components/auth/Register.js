2fc69fba2020f3651aeab8dfb87db290d77682dc
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2020-03-26 19:19:53 +0800

    register user request moved to auth action, connecting the component to the action

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,113 +1,134 @@
-import React, {  useState } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
+// import axios from 'axios';
+import {connect} from 'react-redux';
+import {register} from '../../actions/auth';
+import PropTypes from 'prop-types';
 
 // @to do: change it to auth modal, pop up instead of separate route
 // @to do later: change the user model with email and password only to match abilionveg existing,
 // move username and profilePicture to profile model to not be part of the auth
 
-const Register = () => {
-{/* the default values  */}
-    const [formData, setFormData] = useState({
-    username: '',
-    email: '',
-    password: '',
-    password2: ''
-  });
- {/*  destructure the formData object  */}
-  const { username, email, password, password2 } = formData;
-{/* e.target any key-value pair for every field change from below inputs */}
-  const onChange = e => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-{/* change action in the form with onSubmit, make sure the passwords match */}
-  const onSubmit = async e =>{
-    e.preventDefault();
-    if(password !== password2){
-      console.log('Passwords do not match');
-    } else {
-      console.log(formData);
-      console.log('Submitted form data');
-      //test request with axios before setting up redux
-      const newUser = {
-          username,
-          email,
-          password
-      }
+const Register = ({register}) => {
+	{
+		/* the default values  */
+	}
+	const [ formData, setFormData ] = useState({
+		username: '',
+		email: '',
+		password: '',
+		password2: ''
+	});
+	{
+		/*  destructure the formData object  */
+	}
+	const {username, email, password, password2} = formData;
+	{
+		/* e.target any key-value pair for every field change from below inputs */
+	}
+	const onChange = (e) => {
+		setFormData({...formData, [e.target.name]: e.target.value});
+	};
+	{
+		/* change action in the form with onSubmit, make sure the passwords match */
+	}
+	const onSubmit = async (e) => {
+		e.preventDefault();
+		if (password !== password2) {
+			console.log('Passwords do not match');
+		} else {
+			//call register action from redux, bringing an object with the data,
+			// can be accessed by bulling from the destructed formData above
+			register({username, email, password});
+			console.log(formData);
+			console.log('Submitted form data');
 
-      try {
-          const config = {
-              headers: {
-                  'Content-Type': 'application/json'
-              }
-          }
-          
-          const body = JSON.stringify(newUser);
-          //send data to /api/users
-          const res = await axios.post('/api/users', body, config);
-          console.log(res.data);
+			// //test request with axios before setting up redux
+			// const newUser = {
+			//     username,
+			//     email,
+			//     password
+			// }
 
-      } catch (error) {
-          console.error(error.response.data);
-      }
-    };
-  };
+			// try {
+			//     const config = {
+			//         headers: {
+			//             'Content-Type': 'application/json'
+			//         }
+			//     }
 
+			//     const body = JSON.stringify(newUser);
+			//     //send data to /api/users
+			//     const res = await axios.post('/api/users', body, config);
+			//     console.log(res.data);
 
-  return (
-    <div className="form-wrap">
-      <h1 className="text-center">Sign Up</h1>
-      <form className="" onSubmit={e => onSubmit(e)}>
-        <div className="form-group">
-          <input
-            type="text"
-            placeholder="Userame"
-            name="username"
-            value={username}
-            onChange={e => onChange(e)}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <input
-            type="email"
-            placeholder="Email "
-            name="email"
-            value={email}
-            onChange={e => onChange(e)}
-            required
-          />
-         
-        </div>
-        <div className="form-group">
-          <input
-            type="password"
-            placeholder="Password"
-            name="password"
-            minLength="8"
-            value={password}
-            onChange={e => onChange(e)}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <input
-            type="password"
-            placeholder="Confirm Password"
-            name="password2"
-            minLength="8"
-            value={password2}
-            onChange={e => onChange(e)}
-            required
-          />
-        </div>
-        <input type="submit" className="btn btn-primary" value="Sign Up" />
-      </form>
-      <p className="text-center">
-        Already have an account? <Link to="/login">Log in </Link>
-      </p>
-    </div>
-  );
+			// } catch (error) {
+			//     console.error(error.response.data);
+			// }
+			// ^ moved to auth action, here a=only calling it register();
+		}
+	};
+
+	return (
+		<div className='form-wrap'>
+			<h1 className='text-center'>Sign Up</h1>
+			<form className='' onSubmit={(e) => onSubmit(e)}>
+				<div className='form-group'>
+					<input
+						type='text'
+						placeholder='Userame'
+						name='username'
+						value={username}
+						onChange={(e) => onChange(e)}
+						// required
+					/>
+				</div>
+				<div className='form-group'>
+					<input
+						type='email'
+						placeholder='Email '
+						name='email'
+						value={email}
+						onChange={(e) => onChange(e)}
+						// required
+					/>
+				</div>
+				<div className='form-group'>
+					<input
+						type='password'
+						placeholder='Password'
+						name='password'
+						minLength='8'
+						value={password}
+						onChange={(e) => onChange(e)}
+						// required
+					/>
+				</div>
+				<div className='form-group'>
+					<input
+						type='password'
+						placeholder='Confirm Password'
+						name='password2'
+						minLength='8'
+						value={password2}
+						onChange={(e) => onChange(e)}
+						// required
+					/>
+				</div>
+				<input type='submit' className='btn btn-primary' value='Sign Up' />
+			</form>
+			<p className='text-center'>
+				Already have an account? <Link to='/login'>Log in </Link>
+			</p>
+		</div>
+	);
 };
 
-export default Register;
+Register.propTypes = {
+	register: PropTypes.func.isRequired,
+	isAuthenticated: PropTypes.bool
+};
+
+//connect the redux register action to the component
+
+export default connect(null, {register})(Register);
