e8a008711f26c5018efb60c2fb59901b04cde6f8
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2020-03-27 11:52:25 +0800

    useEfect to save the curent user to the local storage once the app loads

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useEffect} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import TopNavbar from './components/layout/TopNavbar';
 import NavMenu from './components/layout/NavMenu';
@@ -9,28 +9,37 @@ import NewRecipePage from './pages/NewRecipePage';
 import ExplorePage from './pages/ExplorePage';
 
 import {Provider} from 'react-redux';
-
 import store from './store';
+import {loadUser} from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 
 import './App.scss';
 
-const App = () => (
-	<Provider store={store}>
-		<Router>
-			<Fragment>
-				<TopNavbar />
-				<NavMenu />
-				<Route exact path='/' component={RecipesPage} />
-				<Switch>
-					<Route exact path='/explore' component={ExplorePage} />
-					<Route exact path='/recipes' component={RecipesPage} />
-					<Route exact path='/recipes/new' component={NewRecipePage} />
-					<Route exact path='/register' component={Register} />
-					<Route exact path='/login' component={Login} />
-				</Switch>
-			</Fragment>
-		</Router>
-	</Provider>
-);
+const App = () => {
+	useEffect(() => {
+		if (localStorage.token) {
+			setAuthToken(localStorage.token);
+			store.dispatch(loadUser());
+		}
+	}, []);
 
+	return (
+		<Provider store={store}>
+			<Router>
+				<Fragment>
+					<TopNavbar />
+					<NavMenu />
+					<Route exact path='/' component={RecipesPage} />
+					<Switch>
+						<Route exact path='/explore' component={ExplorePage} />
+						<Route exact path='/recipes' component={RecipesPage} />
+						<Route exact path='/recipes/new' component={NewRecipePage} />
+						<Route exact path='/register' component={Register} />
+						<Route exact path='/login' component={Login} />
+					</Switch>
+				</Fragment>
+			</Router>
+		</Provider>
+	);
+};
 export default App;
