ff827df22f74dff600a4628b34b7aac971e7b695
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2020-03-27 11:59:01 +0800

    connecting the redux login action to the login component, login works

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,60 +1,69 @@
-import React, {  useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
+
+import {connect} from 'react-redux';
+import {login} from '../../actions/auth';
+import PropTypes from 'prop-types';
 
 // @to do: change it to auth modal, pop up instead of separate route
 // login with email & password only, to match abilionveg existing method,
 // username and profilePicture will be moved from the user model to the profile model
 
-const Login = () => {
-  const [formData, setFormData] = useState({
-    email: '',
-    password: ''
-  });
+const Login = ({login}) => {
+	const [ formData, setFormData ] = useState({
+		email: '',
+		password: ''
+	});
 
-  const { email, password } = formData;
+	const {email, password} = formData;
 
-  const onChange = e => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
+	const onChange = (e) => {
+		setFormData({...formData, [e.target.name]: e.target.value});
+	};
 
-  const onSubmit = async e =>{
-    e.preventDefault();
-    console.log('Successfully logged in!!');
-  };
+	const onSubmit = async (e) => {
+		e.preventDefault();
+		login({email, password});
+		console.log('Successfully logged in!!');
+	};
 
-
-  return (
-    <div className="form-wrap">
-      <h1 className="text-center">Sign In</h1>
-      <form className="form" onSubmit={e => onSubmit(e)}>
-        <div className="form-group">
-          <input
-            type="email"
-            placeholder="Email Address"
-            name="email"
-            value={email}
-            onChange={e => onChange(e)}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <input
-            type="password"
-            placeholder="Password"
-            name="password"
-            minLength="8"
-            value={password}
-            onChange={e => onChange(e)}
-            required
-          />
-        </div>
-        <input type="submit" className="btn btn-primary" value="Login" />
-      </form>
-      <p className="text-center">
-        Don't have an account? <Link to="/register">Sign Up</Link>
-      </p>
-    </div>
-  );
+	return (
+		<div className='form-wrap'>
+			<h1 className='text-center'>Sign In</h1>
+			<form className='form' onSubmit={(e) => onSubmit(e)}>
+				<div className='form-group'>
+					<input
+						type='email'
+						placeholder='Email Address'
+						name='email'
+						value={email}
+						onChange={(e) => onChange(e)}
+						required
+					/>
+				</div>
+				<div className='form-group'>
+					<input
+						type='password'
+						placeholder='Password'
+						name='password'
+						minLength='8'
+						value={password}
+						onChange={(e) => onChange(e)}
+						required
+					/>
+				</div>
+				<input type='submit' className='btn btn-primary' value='Login' />
+			</form>
+			<p className='text-center'>
+				Don't have an account? <Link to='/register'>Sign Up</Link>
+			</p>
+		</div>
+	);
 };
 
-export default Login;
+Login.propTypes = {
+	login: PropTypes.func.isRequired,
+	isAuthenticated: PropTypes.bool
+};
+
+export default connect(null, {login})(Login);
