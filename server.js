8087dc89bf352135ea261bece67a4ba067962e85
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2020-04-03 13:54:44 +0800

    preparing deployment

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
-
 const app = express();
-
 const connectDB = require('./config/db');
+const path = require('path');
 
 connectDB();
 
@@ -13,8 +12,6 @@ app.use(
     })
 );
 
-app.get('/', (req, res) => res.send(`API Running`));
-
 
 //define routes
 app.use('/api/users', require('./routes/api/users'));
@@ -22,6 +19,16 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/recipes', require('./routes/api/recipes'));
 
+
+//static assets in production
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+
+    });
+}
 
 const PORT = process.env.PORT || 9000;
 
